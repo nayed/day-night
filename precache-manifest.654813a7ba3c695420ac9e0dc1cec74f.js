3e89c8a51ea2cfbaca88c37b640cc9e1a1a72008
Nayed Saïd Ali <nayed.saidali@gmail.com>
2019-02-12 14:48:39 +0100

    Updates

---
+++ precache-manifest.654813a7ba3c695420ac9e0dc1cec74f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b21e5e6784aae442d57",
+    "url": "/day-night/static/js/runtime~main.9b21e5e6.js"
+  },
+  {
+    "revision": "7dbc73f8bfaf74f94993",
+    "url": "/day-night/static/js/main.7dbc73f8.chunk.js"
+  },
+  {
+    "revision": "9179d2ee259db1db2edb",
+    "url": "/day-night/static/js/1.9179d2ee.chunk.js"
+  },
+  {
+    "revision": "f033ef7e734989db7d6aa32b22ed0667",
+    "url": "/day-night/index.html"
+  }
+];
